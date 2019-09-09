630088c375052001a2abcadfa16e150eb75a3a90
andrius <ageciauskas@digisoft.tv>
2019-09-09 21:57:46 +0100

    minor changes

--- src/main/java/com/emploc/model/Person.java
+++ src/main/java/com/emploc/model/Person.java
@@ -1,11 +1,12 @@
 package com.emploc.model;
 
-import com.emploc.model.person.Personroot;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import com.emploc.model.person.Personroot;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
@@ -14,10 +15,10 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
 
 @Getter
 @Setter
-@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
-@ToString
-@EqualsAndHashCode
+@NoArgsConstructor
 @Document(collection = "persons")
 @JsonInclude(NON_EMPTY)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -30,7 +31,7 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
         "pdepartment",
         "pseatNo"
 })
-public class Person  extends Personroot implements Serializable {
+public class Person extends Personroot implements Serializable {
 
     private static final long serialVersionUID = -1023200559156535845L;
 
