b7914bca1c3187badaf89e71c861c1e49a8eb057
ageciauskas <andriusgeci@gmail.com>
2019-09-19 20:23:55 +0100

    adding swagger

--- src/main/java/com/emploc/config/JerseyConfig.java
+++ src/main/java/com/emploc/config/JerseyConfig.java
@@ -4,29 +4,22 @@ import com.emploc.service.rs.PersonRestServiceImpl;
 import com.emploc.validation.BadRequestMapper;
 import com.emploc.validation.EntityNotFoundMapper;
 import com.emploc.validation.ValidationExceptionMapper;
-import io.swagger.jaxrs.config.BeanConfig;
-import io.swagger.jaxrs.config.SwaggerConfigLocator;
-import io.swagger.jaxrs.config.SwaggerContextService;
-import io.swagger.jaxrs.listing.ApiListingResource;
-import io.swagger.jaxrs.listing.SwaggerSerializers;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import javax.ws.rs.ApplicationPath;
 
 @Component
 @ApplicationPath("/")
 public class JerseyConfig extends ResourceConfig {
 
-    @Value("${server.contextPath}")
-    private String serverContextPath;
+    /*@Value("${server.contextPath}")*/
+    private String serverContextPath="/";
 
-    @PostConstruct
+   /* @PostConstruct
     private void configure() {
         this.configureSwagger();
-    }
+    }*/
 
     public JerseyConfig() {
         register(PersonRestServiceImpl.class);
@@ -35,19 +28,24 @@ public class JerseyConfig extends ResourceConfig {
         register(ValidationExceptionMapper.class);
     }
 
-    private void configureSwagger() {
-        this.register(ApiListingResource.class);
-        this.register(SwaggerSerializers.class);
-        BeanConfig swaggerConfig = new BeanConfig();
-        swaggerConfig.setConfigId("springboot-jersey-swagger");
-        swaggerConfig.setTitle("swaggerServiceTitle");
-        swaggerConfig.setVersion("apiVersion");
-        swaggerConfig.setContact("swaggerApiContact");
-        swaggerConfig.setSchemes(new String[]{"http", "https"});
-        swaggerConfig.setBasePath(this.serverContextPath);
-        swaggerConfig.setResourcePackage(JerseyConfig.class.getPackage().getName());
-        swaggerConfig.setPrettyPrint(true);
-        swaggerConfig.setScan(true);
-        SwaggerConfigLocator.getInstance().putConfig(SwaggerContextService.CONFIG_ID_DEFAULT, swaggerConfig);
-    }
+
+    /*OpenAPI oas = new OpenAPI();
+
+    Info info = new Info()
+            .title("Swagger Sample App bootstrap code")
+            .description("This is a sample server Petstore server.  You can find out more about Swagger " +
+                    "at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, " +
+                    "you can use the api key `special-key` to test the authorization filters.")
+            .termsOfService("http://swagger.io/terms/")
+            .contact(new Contact()
+                    .email("apiteam@swagger.io"))
+            .license(new License()
+                    .name("Apache 2.0")
+                    .url("http://www.apache.org/licenses/LICENSE-2.0.html"));
+
+    oas
+*/
+
+
+
 }
